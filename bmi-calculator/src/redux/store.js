35e3a491148124776158b5cfaf6ea2f1132d1bb4
bhodi-creator <nithinjayaraj23@gmail.com>
2024-03-11 19:09:19 +0530

    coomt3

--- bmi-calculator/src/redux/store.js
+++ bmi-calculator/src/redux/store.js
@@ -1,5 +1,5 @@
 import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
-import thunk from "redux-thunk";
+import {thunk} from "redux-thunk";
 import homeReducer from "./Reducers/homeReducer";
 import quizReducer from "./Reducers/quizReducer";
 
